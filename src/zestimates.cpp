0c3552710557dbb09c0e188c6f0a6100c9772ea5
SergeyZaz <s.z.1980@yandex.ru>
2022-05-13 15:54:33 +0300

    +

--- src/zestimates.cpp
+++ src/zestimates.cpp
@@ -1,5 +1,7 @@
 #include "zestimates.h"
 #include "zmessager.h"
+#include "zsettings.h"
+#include <QMessageBox>
 
 ZEstimates::ZEstimates(QWidget* parent, Qt::WindowFlags flags) : ZMdiChild(parent, flags)
 {
@@ -28,6 +30,20 @@ ZEstimatesForm::ZEstimatesForm(QWidget* parent, Qt::WindowFlags flags) : ZEditAb
 	ui.setupUi(this);
 	connect(ui.cmdSave, SIGNAL(clicked()), this, SLOT(applyChanges()));
 	connect(ui.cmdSaveNew, SIGNAL(clicked()), this, SLOT(addNewSlot()));
+	
+	ui.treePosts->setItemDelegate(new ZEstimatesTreeDelegate(this, "posts", ui.treePosts));
+	connect(ui.cmdAddPosts, SIGNAL(clicked()), this, SLOT(addRowSlot()));
+	connect(ui.cmdDelPosts, SIGNAL(clicked()), this, SLOT(delRowSlot()));
+	connect(ui.treePosts, SIGNAL(itemChanged(QTreeWidgetItem*, int)), this, SLOT(itemChangedSlot(QTreeWidgetItem*, int)));
+	ui.treePosts->setColumnWidth(0, 200);
+
+
+	ui.treeWorks->setItemDelegate(new ZEstimatesTreeDelegate(this, "works", ui.treeWorks));
+	connect(ui.cmdAddWorks, SIGNAL(clicked()), this, SLOT(addRowSlot()));
+	connect(ui.cmdDelWorks, SIGNAL(clicked()), this, SLOT(delRowSlot()));
+	connect(ui.treeWorks, SIGNAL(itemChanged(QTreeWidgetItem*, int)), this, SLOT(itemChangedSlot(QTreeWidgetItem*, int)));
+	ui.treeWorks->setColumnWidth(0, 200);
+
 }
 
 ZEstimatesForm::~ZEstimatesForm() {}
@@ -35,7 +51,36 @@ ZEstimatesForm::~ZEstimatesForm() {}
 int ZEstimatesForm::init(const QString& table, int id)
 {
 	ZEditAbstractForm::init(table, id);
-	
+
+	QSqlQuery query;
+	QString stringQuery;
+		
+	for (int i = 0; i < 2; i++)
+	{
+		stringQuery = QString("SELECT id, name, parent_id FROM %1").arg(i == 0 ? "posts" : "works");
+
+		if (query.exec(stringQuery))
+		{
+			while (query.next())
+			{
+				linkInfo elem;
+				elem.id = query.value(0).toInt();
+				elem.name = query.value(1).toString();
+				elem.parent_id = query.value(2).toInt();
+				if(i == 0)
+					mapPosts.insert(elem.id, elem);
+				else
+					mapWorks.insert(elem.id, elem);
+			}
+		}
+	}
+
+
+	loadTblInfo("estimates_posts", ui.treePosts);
+	loadTblInfo("estimates_works", ui.treeWorks);
+
+	updateSumm();
+
 	loadItemsToComboBox(ui.cboContract, "contracts");
 
 	QDate tmp_d = QDate::currentDate();
@@ -51,10 +96,9 @@ int ZEstimatesForm::init(const QString& table, int id)
 	}
 
 	// execute request
-	QString stringQuery = QString("SELECT name,comment,d_open,d_close,contract_id FROM estimates WHERE id = %1")
+	stringQuery = QString("SELECT name,comment,d_open,d_close,contract_id FROM estimates WHERE id = %1")
 		.arg(curEditId);
 
-	QSqlQuery query;
 	bool result = query.exec(stringQuery);
 	if (result)
 	{
@@ -113,6 +157,33 @@ void ZEstimatesForm::applyChanges()
 			return;
 	}
 
+	//сохраняю информацию из таблиц
+	QTreeWidget* tree;
+	for (int i = 0; i < 2; i++)
+	{
+		stringQuery = QString("DELETE FROM %1 WHERE estimate_id=%2").arg(i==0 ? "estimates_posts" : "estimates_works").arg(curEditId);
+		if (!query.exec(stringQuery))
+			ZMessager::Instance().Message(_CriticalError, query.lastError().text());
+
+		tree = (i == 0) ? ui.treePosts : ui.treeWorks;
+
+		QTreeWidgetItemIterator it(tree);
+		while (*it)
+		{
+			stringQuery = QString("INSERT INTO %1 (estimate_id,link_id,count,val) VALUES (%2, %3, %4, %5)")
+				.arg(i == 0 ? "estimates_posts" : "estimates_works")
+				.arg(curEditId)
+				.arg((*it)->data(0, Qt::UserRole).toInt())
+				.arg((*it)->data(1, Qt::DisplayRole).toInt())
+				.arg((*it)->data(2, Qt::DisplayRole).toDouble());
+
+			if (!query.exec(stringQuery))
+				ZMessager::Instance().Message(_CriticalError, query.lastError().text());
+
+			++it;
+		}
+	}
+
 	accept();
 }
 
@@ -122,4 +193,259 @@ int ZEstimatesForm::copyData(int curId, int newId)
 	QString stringQuery;
 
 	return 1;
+}
+
+void ZEstimatesForm::updateSumm()
+{
+	int count = 0, summ = 0;
+	QTreeWidgetItem* pItem;
+
+	QTreeWidget* tree = ui.treeWorks;
+
+	for (int i = 0; i < 2; i++)
+	{
+		if(i==1)
+			tree = ui.treePosts;
+
+		QTreeWidgetItemIterator it(tree);
+		while (*it) 
+		{
+			if(!(*it)->parent())
+				count += (*it)->text(1).toInt();
+			summ += (*it)->text(3).toInt();
+			++it;
+		}
+	}
+
+	ui.lblTotal->setText(QString("Итого: человек: %1, сумма: %2").arg(count).arg(summ));
+}
+
+void ZEstimatesForm::addRowSlot()
+{
+	QTreeWidget* tree = ui.treeWorks;
+	if (sender() == ui.cmdAddPosts)
+		tree = ui.treePosts;
+
+	QTreeWidgetItem* pItem = new QTreeWidgetItem(tree);
+	pItem->setText(1, "1");
+	pItem->setText(2, "1000");
+	pItem->setText(3, "1000");
+	pItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsEnabled);
+	pItem->setSizeHint(0, QSize(150, 25));
+	tree->addTopLevelItem(pItem);
+	updateSumm();
+}
+
+void ZEstimatesForm::delRowSlot()
+{
+	QTreeWidget* tree = ui.treeWorks;
+	if (sender() == ui.cmdAddPosts)
+		tree = ui.treePosts;
+	
+	QTreeWidgetItem *curItem = tree->currentItem();
+	if (!curItem)
+		return;
+	if (curItem->parent())
+		return;
+
+	if (QMessageBox::question(this, "Внимание", "Вы действительно хотите удалить выделенную строку?", "Да", "Нет", QString::null, 0, 1) != 0)
+		return;
+
+	delete curItem;
+	updateSumm();
+}
+
+int ZEstimatesForm::loadTblInfo(const QString& tbl, QTreeWidget* tree)
+{
+	tree->blockSignals(true);
+
+	tree->clear();
+
+	QMap<int, linkInfo>* pMap = (tbl == "estimates_posts") ? &mapPosts : &mapWorks;
+
+	QSqlQuery query;
+	QString stringQuery;
+	QTreeWidgetItem* pItem;
+
+	stringQuery = QString("SELECT link_id, count, val FROM %1 WHERE estimate_id=%2").arg(tbl).arg(curEditId);
+	if (!query.exec(stringQuery))
+	{
+		ZMessager::Instance().Message(_CriticalError, query.lastError().text(), tr("Ошибка"));
+	}
+	else
+	{
+		while(query.next())
+		{
+			pItem = new QTreeWidgetItem(tree);
+			int idx = query.value(0).toInt();
+			pItem->setData(0, Qt::UserRole, idx);
+			pItem->setText(0, pMap->value(idx).name);
+			int v1 = query.value(1).toInt();
+			pItem->setData(1, Qt::DisplayRole, v1);
+			int v2 = query.value(2).toInt();
+			pItem->setData(2, Qt::DisplayRole, v2);
+			pItem->setData(3, Qt::DisplayRole, v1 * v2);
+			pItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsEnabled);
+			pItem->setSizeHint(0, QSize(150, 25));
+			tree->addTopLevelItem(pItem);
+		}
+	}
+
+	QTreeWidgetItem* pParentItem;
+	int p_id, id;
+	for (int i = 0; i < tree->topLevelItemCount(); i++)
+	{
+		pItem = tree->topLevelItem(i);
+		id = pItem->data(0, Qt::UserRole).toInt();
+		p_id = pMap->value(id).parent_id;
+		if (p_id == 0)
+			continue;
+
+		for (int j = 0; j < tree->topLevelItemCount(); j++)
+		{
+			if (i == j)
+				continue;
+			pParentItem = tree->topLevelItem(j);
+			if (pParentItem->data(0, Qt::UserRole).toInt() == p_id)
+			{
+				pItem = tree->takeTopLevelItem(i);
+				pItem->setText(1, "");
+				pParentItem->insertChild(pParentItem->childCount(), pItem);
+				i--;
+				break;
+			}
+		}
+	}
+	tree->expandAll();
+	tree->blockSignals(false);
+	return 1;
+}
+
+void ZEstimatesForm::itemChangedSlot(QTreeWidgetItem* item, int column)
+{
+	QTreeWidget* tree = dynamic_cast<QTreeWidget*>(sender());
+	if (!tree || column != 0)
+	{
+		updateSumm();
+		return;
+	}
+
+	while(item->childCount())
+		delete item->takeChild(0);
+
+	int indx = item->data(0, Qt::UserRole).toInt();
+	if (indx == 0)
+		return;
+
+	QMap<int, linkInfo>* pMap = (tree == ui.treePosts) ? &mapPosts : &mapWorks;
+	QMap<int, linkInfo>::const_iterator iT = pMap->constBegin();
+	while (iT != pMap->constEnd())
+	{
+		auto v = iT.value();
+		if (v.parent_id == indx)//добавляю!
+		{
+			QTreeWidgetItem* pItem = new QTreeWidgetItem(item);
+			pItem->setData(0, Qt::UserRole, v.id);
+			pItem->setText(0, v.name);
+			pItem->setData(2, Qt::DisplayRole, 0);
+			pItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable | Qt::ItemIsEnabled);
+			pItem->setSizeHint(0, QSize(150, 25));
+			item->addChild(pItem);
+		}
+		
+		++iT;
+	}
+
+	tree->expandAll();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+ZEstimatesTreeDelegate::ZEstimatesTreeDelegate(ZEstimatesForm* p, const QString& tbl, QObject* parent)
+	: QItemDelegate(parent), pParent(p), mTblName(tbl)
+{
+}
+
+QWidget* ZEstimatesTreeDelegate::createEditor(QWidget* parent,
+	const QStyleOptionViewItem& option,
+	const QModelIndex& index) const
+{
+	if (ZSettings::Instance().f_ReadOnly || ZSettings::Instance().m_UserType == 1)
+		return NULL;
+
+	int column = index.column();
+
+	QModelIndex p = index.parent();
+	if(p.isValid() && column!=2)
+		return NULL;
+
+	if (column == 0)
+	{
+		QComboBox *w = new QComboBox(parent);
+		w->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+		return w;
+	}
+	if (column == 1 || column == 2)
+	{
+		QSpinBox *w = new QSpinBox(parent);
+		w->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Maximum);
+		w->setMinimum(0);
+		w->setMaximum(1000000);
+		return w;
+	}
+
+	return NULL;
+}
+
+void ZEstimatesTreeDelegate::setEditorData(QWidget* editor,
+	const QModelIndex& index) const
+{
+	QSpinBox* spin = dynamic_cast<QSpinBox*>(editor);
+	if (spin)
+	{
+		spin->setValue(index.model()->data(index, Qt::DisplayRole).toInt());
+		return;
+	}
+	QComboBox* cbo = dynamic_cast<QComboBox*>(editor);
+	if (cbo)
+	{
+		loadItemsToComboBox(cbo, mTblName, "parent_id=0");
+		int idx = index.model()->data(index, Qt::UserRole).toInt();
+		idx = cbo->findData(idx);
+		cbo->setCurrentIndex(idx);
+		return;
+	}
+	QItemDelegate::setEditorData(editor, index);
+}
+
+void ZEstimatesTreeDelegate::setModelData(QWidget* editor, QAbstractItemModel* model,
+	const QModelIndex& index) const
+{
+	QSpinBox* spin = dynamic_cast<QSpinBox*>(editor);
+	if (spin)
+	{
+		int v = spin->value();
+		model->setData(index, v, Qt::EditRole);
+
+		int column = index.column() == 1 ? 2 : 1;
+		int v2 = model->data(model->index(index.row(), column, index.parent()), Qt::DisplayRole).toInt();
+		model->setData(model->index(index.row(), 3, index.parent()), v * v2, Qt::DisplayRole);
+		return;
+	}
+	QComboBox* cbo = dynamic_cast<QComboBox*>(editor);
+	if (cbo)
+	{
+		QString txt = cbo->currentText();
+		int indx = cbo->itemData(cbo->findText(txt), Qt::UserRole).toInt();
+
+		model->setData(index, txt, Qt::EditRole);
+		model->setData(index, indx, Qt::UserRole);
+		return;
+	}
+	QItemDelegate::setModelData(editor, model, index);
+}
+
+void ZEstimatesTreeDelegate::updateEditorGeometry(QWidget* editor,
+	const QStyleOptionViewItem& option, const QModelIndex& index) const
+{
+	editor->setGeometry(option.rect);
 }
--- src/zestimates.h
+++ src/zestimates.h
@@ -4,9 +4,17 @@
 #include "ui_zestimatesform.h"
 #include "zeditbaseform.h"
 
+#include <QTreeWidget>
+#include <QItemDelegate>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QMap>
+
 class ZEstimatesForm : public ZEditAbstractForm
 {
 	Q_OBJECT
+		
+	int loadTblInfo(const QString& tbl, QTreeWidget *tree);
 
 public:
 	ZEstimatesForm(QWidget* parent, Qt::WindowFlags flags = 0);
@@ -14,11 +22,24 @@ public:
 
 	int init(const QString& table, int id);
 	int copyData(int curId, int newId);
+	void updateSumm();
+
 	Ui::ZEstimatesForm ui;
+
+	struct linkInfo
+	{
+		int id;
+		QString name;
+		int parent_id;
+	};
+	QMap<int, linkInfo> mapPosts, mapWorks;
 
 protected slots:
 	void applyChanges();
 	void addNewSlot();
+	void addRowSlot();
+	void delRowSlot();
+	void itemChangedSlot(QTreeWidgetItem*, int);
 };
 
 class ZEstimates : public ZMdiChild
@@ -28,4 +49,24 @@ public:
 	ZEstimates(QWidget* parent, Qt::WindowFlags flags = 0);
 
 	void init(const QString& m_TblName);
+};
+
+
+class ZEstimatesTreeDelegate : public QItemDelegate
+{
+	QString mTblName;
+	ZEstimatesForm* pParent;
+public:
+	ZEstimatesTreeDelegate(ZEstimatesForm *p, const QString& tbl, QObject* parent = 0);
+
+	QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option,
+		const QModelIndex& index) const;
+
+	void setEditorData(QWidget* editor, const QModelIndex& index) const;
+
+	void setModelData(QWidget* editor, QAbstractItemModel* model,
+		const QModelIndex& index) const;
+
+	void updateEditorGeometry(QWidget* editor,
+		const QStyleOptionViewItem& option, const QModelIndex& index) const;
 };
